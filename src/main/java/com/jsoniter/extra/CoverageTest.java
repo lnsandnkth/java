aa6cc4939fe9e1a16d77625d17999354d126c573
Noah Rahimzadagan <noah.rahimzadagan@soderbergpartners.se>
2022-02-23 14:50:43 +0100

    coverage test added

---
+++ src/main/java/com/jsoniter/extra/CoverageTest.java
@@ -0,0 +1,29 @@
+package com.jsoniter.extra;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CoverageTest {
+
+    public static boolean[] coverageArray = new boolean[18];
+
+    public static void writeToFile() throws IOException, IOException {
+        String outputString = "";
+        int coveragePositives = 0;
+
+        for (int i = 0; i<coverageArray.length;i++) {
+            if (coverageArray[i]) {
+                coveragePositives++;
+            }
+            outputString=outputString+"Statement "+ i+ " is covered: " + coverageArray[i] + "\n";
+
+        }
+
+        outputString=outputString+ "Coverage ratio: " + coveragePositives+"/"+coverageArray.length;
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("coverage3.txt"));
+        writer.write(outputString);
+        writer.close();
+    }
+}
